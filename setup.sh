67be443740c65a3d348ce9baea7d52ca01e7c42c
John Hinnegan <john.hinnegan@gmail.com>
2012-02-14 14:47:53 -0800

    add ssh setup

--- setup.sh
+++ setup.sh
@@ -21,6 +21,10 @@ echo "created symlink to gitconfig"
 ln -sf $currDir/gemrc ~/.gemrc
 echo "created symlink to gemrc"
 
+mv ~/.ssh ~/ssh_bk
+ln -sf $currDir/ssh ~/.ssh
+echo "Created symlink to ssh (existing contents of .ssh in ~/ssh_bk)"
+
 # prepare the vimrc
 cp $directory/etc/vim/vimrc $currDir/vimrc 
 sed '/.*LongLineWarning.*/d' $currDir/vimrc > $currDir/vimrc.new 
@@ -43,4 +47,5 @@ echo "overwrite ir_black color with the good one"
 if which seq > /dev/null; then
   echo "seq not found in path.  some functions may not work.  see README"
 fi
+
 
